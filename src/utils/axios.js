dc46540de8c9725b31c284be260b7bd4c1e2620a
maged <magedelhossary663@gmail.com>
2026-04-30 16:21:50 +0400

    update frontend

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://backend-women-ecommerce.onrender.com",
+  baseURL: "https://backend-women-ecommerce.onrender.com/api", 
 });
 
 instance.interceptors.request.use((config) => {
